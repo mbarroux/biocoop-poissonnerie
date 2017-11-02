bfffc10821dec1b387ea6b015ede3a6975454fa5
mbarroux <mickael.barroux@gmail.com>
2017-11-02 10:52:48 +0100

    Ajout de l'annotation ResponseBody pour avoir du json en sortie

--- src/main/java/fr/biocoop/poissonnerie/controllers/PoissonnerieController.java
+++ src/main/java/fr/biocoop/poissonnerie/controllers/PoissonnerieController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,7 @@ public class PoissonnerieController {
     }
 
     @RequestMapping("/poisson/findByNom")
-        //@ResponseBody // TODO ?
+    @ResponseBody // TODO ?
     List<Poisson> findPoissonsByNom(@RequestParam("nom") String nom) {
         List<Poisson> poissons = poissonnerieRepository.findByEspeceLike(nom);
         return poissons;
